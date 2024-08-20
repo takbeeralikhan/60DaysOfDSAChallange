979a81bf1a36744dc9f2e7275fe02e97151bd8ee
takbeerali <takbeeraliwork@gmail.com>
2024-08-20 14:40:27 +0500

    update: add 2d array is added

--- 60days.js
+++ 60days.js
@@ -1,14 +1,14 @@
-let array = [1, 2, 3, 4, 5, 6];
+// let array = [1, 2, 3, 4, 5, 6];
 
-console.log(sumOfAllElementInArray(array));
+// console.log(sumOfAllElementInArray(array));
 
-function sumOfAllElementInArray(arr) {
-  let sum = 0;
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum;
-}
+// function sumOfAllElementInArray(arr) {
+//   let sum = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
 // function reverseArray(arr){
 
 //     let reverseArray = [];
@@ -35,3 +35,59 @@ function sumOfAllElementInArray(arr) {
 //     }
 //     return {max, min};
 // }
+
+// print 2D dimenstional array
+
+// const matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// console.log("length of matrix", matrix.length, matrix[0].length);
+
+// for (let i = 0; i < matrix.length; i++) {
+//   for (let j = 0; j < matrix[i].length; j++) {
+//     console.log(matrix[i][j]);
+//   }
+// }
+
+//****************Add 2D arrays***************
+function addMatrices(matrixA, matrixB) {
+  // Get the number of rows and columns
+  const rows = matrixA.length;
+  const cols = matrixA[0].length;
+
+  // Create a new matrix to store the result
+  const result = [];
+
+  // Loop through each element and add corresponding elements
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      // Add corresponding elements
+      row.push(matrixA[i][j] + matrixB[i][j]);
+    }
+    result.push(row);
+  }
+
+  return result;
+}
+
+const matrixA = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const matrixB = [
+  [9, 8, 7],
+  [6, 5, 4],
+  [3, 2, 1],
+];
+
+// Add the matrices
+const sumMatrix = addMatrices(matrixA, matrixB);
+
+// Display the result
+console.log(sumMatrix);
